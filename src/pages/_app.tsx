a25b18e780d9981d62f8466ffadb66885dac21e1
Maulana Sodiqin <maulanasdqn@gmail.com>
2023-01-14 18:54:50 +0700

    Maulana Sodiqin | Initial Setting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,35 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps } from "next/app";
+
+import { ThemeProvider } from "@mui/material/styles";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+
+import Head from "next/head";
+import theme from "@/utilities/common/theme";
+import CssBaseline from "@mui/material/CssBaseline";
+import createEmotionCache from "@/utilities/common/createEmotionCache";
+
+const clientSideEmotionCache = createEmotionCache();
+
+interface GlobalProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+export default function App(props: GlobalProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />;
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
